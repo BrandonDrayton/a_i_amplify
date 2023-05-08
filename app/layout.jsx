caccadcbde667deeb65a9641af70d6b9b2d82acb
BrandonDrayton <brandondrayton@gmail.com>
2023-05-07 23:36:17 -0400

    File and Styling structure, Assets.

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import '@styles/global.css';
+
+export const metadata = {
+    title: 'A.I. Amplify',
+    description: 'Discover and share A.I. Prompts'
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'><body>
+        <div className='main'>
+            <div className='gradient'/>
+        </div>
+        <main className='app'>{children}</main>
+    </body></html>
+  )
+}
+
+export default RootLayout
