c6139ba2c3a5309393e027af28c9e5be4a4a41dd
BrandonDrayton <brandondrayton@gmail.com>
2023-07-13 00:05:38 -0400

    create prompt connected to db

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,15 +5,39 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
+import { Router } from "next/router";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
   const [submit, setSubmit] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
 
-  const createPrompt = async (e) => {};
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmit(true);
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        Router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmit(false);
+    }
+  };
 
   return (
     <Form
