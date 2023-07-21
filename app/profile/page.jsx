971388efe5afddc31af073a349af6ee589295300
BrandonDrayton <brandondrayton@gmail.com>
2023-07-20 20:39:08 -0400

    User_Prompt_ID Profile component w id routes

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,6 +5,17 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+  const fetchPosts = async () => {
+    const response = await fetch(`/api/users/${session?.user.id}/posts`);
+    const data = await response.json();
+    setPosts(data);
+  };
+  useEffect(() => {
+    console.log(posts);
+    fetchPosts();
+  }, []);
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
   };
@@ -32,7 +43,7 @@ const MyProfile = () => {
       <Profile
         name="My"
         description="Welcome to your personalized profile page"
-        data={[]}
+        data={posts}
         handleEdit={handleEdit}
         handleDelete={handleDelete}
       />
